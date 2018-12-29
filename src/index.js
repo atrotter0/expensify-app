bc3d44a31344430c828686dd92a42163f4051d8d
Abel Trotter <a_trotter0@hotmail.com>
2018-12-28 17:04:11 -0800

    Add ExpenseListItem, render expenses using selector

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { addExpense } from './actions/expenses';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import './styles/styles.scss';
 
 const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: 1000 }));
 
 const jsx = (
   <Provider store={store}>
