45e9ea47dcb243c1accf249d1863b6e74b2df6af
Abel Trotter <a_trotter0@hotmail.com>
2018-12-28 16:36:34 -0800

    Import Provider, add ExpenseList and HOC connect

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
 
 const ExpenseDashboardPage = () => {
   return(
     <div>
-      <p>This is expense dashboard component</p>
+      <ExpenseList />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import './styles/styles.scss';
 
 const store = configureStore();
 
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
 ReactDOM.render(
-  <AppRouter />,
+  jsx,
   document.getElementById('root')
 );
