780e20c3cf5c935bd4bcf36ed710c200d6c2c10e
Abel Trotter <a_trotter0@hotmail.com>
2018-12-31 13:02:10 -0800

    Call addExpense action generator, add AddExpensePage tests

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ExpenseForm from './ExpenseForm';
 import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
 const EditExpensePage = props => {
